8805e786d62914ac506f1ee71ae0552bf3b9ca10
Error7799 <kingy7799sentamu@gmail.com>
2024-09-15 16:52:51 +0300

    GG

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
-    pass
+    
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    
+    
+    def __init__(self, name, pet_type, owner = None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is a invalid pet type it has to be one of these {Pet.PET_TYPES}")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        
+    def pets(self):
+        return[pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception(f"{pet} is a invalid pet type")
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+            
+        pass
+         
